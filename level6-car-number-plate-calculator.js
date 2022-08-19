6ff661cf17c5bb0a05f03822a55fc02a08d308c6
Gary Malcolm Bleasdale <gary@bleasdale.dev>
2022-08-19 13:01:57 -0300

    amended kata car number plate calculator

--- level6-car-number-plate-calculator.js
+++ level6-car-number-plate-calculator.js
@@ -1,19 +1,24 @@
-function code(strng) {
-  let binaryArray =[]
-  while (strng>0){  
-    let remainder = strng % 2
-    binaryArray.push(remainder) 
-    strng = Math.floor(strng/2)    
+// the alphabet: 'abcdefghijklmnopqrstuvwxyz'
+function findTheNumberPlate(customerID){
+  let alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  let thirdLetterIndex=Math.floor(customerID/675324)
+  let thirdLetter = alphabetArray[thirdLetterIndex]
+  let secondLetterValue = customerID%675324
+  let secondLetterIndex=Math.floor(secondLetterValue/25974)
+  let secondLetter = alphabetArray[secondLetterIndex]
+  let firstLetterValue = secondLetterValue%25974
+  let firstLetterIndex=Math.floor(firstLetterValue/999)
+  let firstLetter = alphabetArray[firstLetterIndex]
+  let lastThreeDigits = firstLetterValue%999 + 1
+  if (lastThreeDigits<10){
+    lastThreeDigits = '0'+'0'+lastThreeDigits.toString()
+  } else if (lastThreeDigits<100){
+    lastThreeDigits = '0'+lastThreeDigits.toString()
+  } else {
+    lastThreeDigits = lastThreeDigits
   }
-  let binaryArr2 = +binaryArray.reverse().join("")
-  return binaryArr2
 
+  return firstLetter + secondLetter + thirdLetter + (lastThreeDigits)
 }
-// function decode(str) {
-//   // your code
-// }
-
-console.log(code(133))
-
 
 
