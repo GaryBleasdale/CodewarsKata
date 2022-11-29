09652006c84cd0814cfb091b61ba24d4a6c179f3
Gary Bleasdale <gary@bleasdale.dev>
2022-11-29 09:33:48 -0300

    commited working, but inefficient, answer to valid palindrome challenge

--- valid_palindrome.js
+++ valid_palindrome.js
@@ -4,35 +4,50 @@
 let isPalindrome = s => {
     alphaArr =[]
     for (e of s){
-    if (e.match(/^[0-9a-zA-Z]+$/)){
-        alphaArr.push(e.toLowerCase())
-        console.log(alphaArr)
-      }
-  }
-    console.log(alphaArr.length)
+        if (e.match(/^[0-9a-zA-Z]+$/)){
+            alphaArr.push(e.toLowerCase())
+            
+        }
+    }
     comparisonArrOne=[]
     comparisonArrTwo=[]
-    for (i=0;i<alphaArr.length/2-1;i++){
-        console.log(i)
-       comparisonArrOne.push(alphaArr[i])
-    }
-    console.log(comparisonArrOne.length)
-    for (i=Math.ceil(alphaArr.length/2);i<alphaArr.length;i++){
-        console.log(i)
-        comparisonArrTwo.push(alphaArr[i])
-    }
-    console.log(comparisonArrOne)
-    console.log(comparisonArrTwo)
-    let reversedComparisonArrTwo = comparisonArrTwo.reverse()
-    console.log(reversedComparisonArrTwo)
-    if(comparisonArrOne.join("") == reversedComparisonArrTwo.join("")){
-        return true
-    } else {
-        return false
+    if (alphaArr.length <3){
+        if (alphaArr[alphaArr.length-1]==alphaArr[0]){
+            return true
+        } else {   
+            return false
+        }
+    } else if (alphaArr.length%2 == 0) {
+        for (i=0;i<alphaArr.length/2;i++){
+           comparisonArrOne.push(alphaArr[i])
+        }
+        for (i=Math.floor(alphaArr.length/2);i<alphaArr.length;i++){
+            comparisonArrTwo.push(alphaArr[i])
+        }
+        let reversedComparisonArrTwo = comparisonArrTwo.reverse()
+
+        if(comparisonArrOne.join("") == reversedComparisonArrTwo.join("")){
+            return true
+        } else {
+            return false
+        }
+    } else {  
+        for (i=0;i<alphaArr.length/2-1;i++){
+        comparisonArrOne.push(alphaArr[i])
+        }
+        for (i=Math.ceil(alphaArr.length/2);i<alphaArr.length;i++){
+            comparisonArrTwo.push(alphaArr[i])
+        }
+        let reversedComparisonArrTwo = comparisonArrTwo.reverse()
+        console.log(reversedComparisonArrTwo)
+        if(comparisonArrOne.join("") == reversedComparisonArrTwo.join("")){
+            return true
+        } else {
+            return false
+        }
     }
   
 }
 
-console.log(isPalindrome("A man, a plan, a canal: Panama")) // true
-console.log(isPalindrome(" ")) //true
-console.log(isPalindrome("aa")) //false
+
+// The code above works - however, for extremely long strings, it will require excess use of computational power and would crash most computers.  A good challenge would be to refactor in order to be able to make it more efficient.
